8a85d52ab5f758e57926cecded54d43fbde288c0
Jonathan Ferrari <jonathanferrari@berkeley.edu>
2022-11-26 15:24:57 -0800

    finish oil notebook

--- oil/oil.py
+++ oil/oil.py
@@ -18,16 +18,26 @@ def load_oil():
     oil["barrels"] = 1000 * oil["barrels"]
     return oil
 
+oil = load_oil()
+    
 def plot_oil():
-    oil = load_oil()
     printmd('<p style="font-size:20px"> Use the dropdown below to select <code>scatter</code> or <code>line</code> to show a scatter plot, or a line plot, respectively! </p>')
     @interact(graph = Dropdown(options = ["scatter", "line"], value = "scatter"))
     def plot(graph):
         f = px.scatter
         if graph == "line":
             f = px.line
-        return f(oil, x = "date", y = "barrels", template = "seaborn")
+        return f(oil, x = "date", y = "barrels", template = "seaborn", title = "Oil Production in the United States")
     
+def oil_line():
+    printmd("<p style = 'font-size: 20px'> Use the checkbox below to show the linear regression line in the plot!</p>")
+    @interact(line = Checkbox(value = False, description = "Show regression line"))
+    def helper(line):
+        if line:
+            px.scatter(oil, x = "date", y = "barrels", template = "seaborn", title = "Oil Production in the United States", trendline = "ols")
+        else:
+            px.scatter(oil, x = "date", y = "barrels", template = "seaborn", title = "Oil Production in the United States")
+        
 def show_history():
     return IFrame("https://www.historycentral.com/Today/21st.html#:~:text=2003%2D%20U.S.%20Invades%20Iraq", width = "100%", height = 800)
 
@@ -41,7 +51,9 @@ def r_scatter(r = None, num = 10):
         fig.update_layout(height = 600, width = 600)
         fig.show()
     else:
-    
+        printmd("<p style = 'font-size: 20px'> Use the slider below to change the correlation between the two variables!</p>")
+        printmd("<p style = 'font-size: 20px'> Use the dropdown to select how many points to plot!</p>")
+        printmd("<p style = 'font-size: 20px'> Use the checkbox to show the linear regression line!</p>")
         @interact(r = FloatSlider(min = -1, max = 1, value = .2, step= .05), n = Dropdown(options = [10, 100, 1000], value = 100), line = Checkbox(value = False, description = "Show regression line"))
         def helper(r, n = 100, line = False):
             x = np.random.normal(0, 1, n)

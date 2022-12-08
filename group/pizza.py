9931ec64fa02d54c49c6a0387c1b3fb148b2424f
Jonathan Ferrari <jonathanferrari@berkeley.edu>
2022-12-07 18:34:36 -0800

    add plotly graph

--- group/pizza.py
+++ group/pizza.py
@@ -1,5 +1,6 @@
 
 import pandas as pd, numpy as np, os
+import plotly.express as px
 from IPython.display import *
 from ipywidgets import *
 
@@ -18,3 +19,19 @@ restraunt = Dropdown(options = list(menus.keys()), value = "dominos")
 
 def pick_data():
     return menus[restraunt.value]
+
+def printmd(s):
+    display(Markdown(s))
+
+def plot_party(pizza_price = 14, drink_price = 2.69, slices = 10, cups = 10):
+    printmd("## Please Allow 20-30 Seconds for the Plot to Load")
+    people, budget = 500, 3000
+    df = pd.DataFrame(columns = ['Pizza', 'Drink', 'Cost'])
+    min_pizza, min_drink = people//slices, people//cups
+    for i in list(range(min_pizza, 201)):
+        for j in list(range(min_drink, 201)):
+            if (cost := i*pizza_price + j*drink_price) <= budget:
+                df = df.append({'Pizza': i, 'Drink': j, 'Cost': cost}, ignore_index = True)
+    fig = px.scatter_3d(df, x = 'Pizza', y = 'Drink', z = 'Cost', color = 'Cost', opacity = 0.5, template = "seaborn")
+    fig = fig.update_layout(margin=dict(l=20, r=20, t=20, b=20), scene_camera = dict(eye=dict(x=1, y=2.5, z=0.1)))
+    return fig
